83711c3d3490f749d98ac7c510be9fac9f35544c
pkmnjourney <38611352+pkmnjourney@users.noreply.github.com>
2024-02-22 20:15:21 +0400

    Add files via upload

---
+++ Task 3/include/texture.h
@@ -0,0 +1,27 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+#define GL_SILENCE_DEPRECATION
+
+#if defined(__APPLE__)
+#include <OpenGL/gl.h>
+#else
+#include <GL/gl.h>
+
+#endif
+#include <string>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+class MyTexture {
+public:
+    int height, width;
+    unsigned int id;
+    GLubyte data[512][512][3];
+
+    MyTexture();
+
+    void from_path(const std::string&);
+};
+
+#endif
